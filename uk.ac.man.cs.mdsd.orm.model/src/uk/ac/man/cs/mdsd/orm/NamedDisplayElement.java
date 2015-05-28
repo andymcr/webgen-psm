0bc055c164597beaa9b47fd0579a86a1e7036788
Andy Carpenter <Andy.Carpenter@manchester.ac.uk>
2015-05-28 15:52:34 +0100

    New (Xcore) code generator

--- uk.ac.man.cs.mdsd.orm.model/src/uk/ac/man/cs/mdsd/orm/NamedDisplayElement.java
+++ uk.ac.man.cs.mdsd.orm.model/src/uk/ac/man/cs/mdsd/orm/NamedDisplayElement.java
@@ -25,29 +25,30 @@ package uk.ac.man.cs.mdsd.orm;
  */
 public interface NamedDisplayElement extends NamedElement {
 	/**
-	 * Returns the value of the '<em><b>Display Label</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Display Label</b></em>' attribute.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Display Label</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Display Label</em>' attribute.
-	 * @see #setDisplayLabel(String)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getNamedDisplayElement_DisplayLabel()
-	 * @model required="true"
-	 * @generated
-	 */
+   * @return the value of the '<em>Display Label</em>' attribute.
+   * @see #setDisplayLabel(String)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getNamedDisplayElement_DisplayLabel()
+   * @model unique="false" required="true"
+   *        annotation="http://www.eclipse.org/emf/2002/GenModel property='None'"
+   * @generated
+   */
 	String getDisplayLabel();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.NamedDisplayElement#getDisplayLabel <em>Display Label</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.NamedDisplayElement#getDisplayLabel <em>Display Label</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Display Label</em>' attribute.
-	 * @see #getDisplayLabel()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Display Label</em>' attribute.
+   * @see #getDisplayLabel()
+   * @generated
+   */
 	void setDisplayLabel(String value);
 
 } // NamedDisplayElement
--- uk.ac.man.cs.mdsd.orm.model/src/uk/ac/man/cs/mdsd/orm/OrmModel.java
+++ uk.ac.man.cs.mdsd.orm.model/src/uk/ac/man/cs/mdsd/orm/OrmModel.java
@@ -37,242 +37,242 @@ import org.eclipse.emf.ecore.EObject;
  */
 public interface OrmModel extends EObject {
 	/**
-	 * Returns the value of the '<em><b>Collection Types</b></em>' containment reference list.
-	 * The list contents are of type {@link uk.ac.man.cs.mdsd.orm.CollectionType}.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Collection Types</b></em>' containment reference list.
+   * The list contents are of type {@link uk.ac.man.cs.mdsd.orm.CollectionType}.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Collection Types</em>' containment reference list isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Collection Types</em>' containment reference list.
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_CollectionTypes()
-	 * @model containment="true"
-	 * @generated
-	 */
+   * @return the value of the '<em>Collection Types</em>' containment reference list.
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_CollectionTypes()
+   * @model containment="true"
+   * @generated
+   */
 	EList<CollectionType> getCollectionTypes();
 
 	/**
-	 * Returns the value of the '<em><b>Database Technology</b></em>' attribute.
-	 * The default value is <code>"MySql"</code>.
-	 * The literals are from the enumeration {@link uk.ac.man.cs.mdsd.orm.DatabaseTechnologies}.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Database Technology</b></em>' attribute.
+   * The default value is <code>"MySql"</code>.
+   * The literals are from the enumeration {@link uk.ac.man.cs.mdsd.orm.DatabaseTechnologies}.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Database Technology</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Database Technology</em>' attribute.
-	 * @see uk.ac.man.cs.mdsd.orm.DatabaseTechnologies
-	 * @see #setDatabaseTechnology(DatabaseTechnologies)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseTechnology()
-	 * @model default="MySql" ordered="false"
-	 * @generated
-	 */
+   * @return the value of the '<em>Database Technology</em>' attribute.
+   * @see uk.ac.man.cs.mdsd.orm.DatabaseTechnologies
+   * @see #setDatabaseTechnology(DatabaseTechnologies)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseTechnology()
+   * @model default="MySql" unique="false" ordered="false"
+   * @generated
+   */
 	DatabaseTechnologies getDatabaseTechnology();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseTechnology <em>Database Technology</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseTechnology <em>Database Technology</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Database Technology</em>' attribute.
-	 * @see uk.ac.man.cs.mdsd.orm.DatabaseTechnologies
-	 * @see #getDatabaseTechnology()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Database Technology</em>' attribute.
+   * @see uk.ac.man.cs.mdsd.orm.DatabaseTechnologies
+   * @see #getDatabaseTechnology()
+   * @generated
+   */
 	void setDatabaseTechnology(DatabaseTechnologies value);
 
 	/**
-	 * Returns the value of the '<em><b>Database Prefix</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Database Prefix</b></em>' attribute.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Database Prefix</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Database Prefix</em>' attribute.
-	 * @see #setDatabasePrefix(String)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabasePrefix()
-	 * @model
-	 * @generated
-	 */
+   * @return the value of the '<em>Database Prefix</em>' attribute.
+   * @see #setDatabasePrefix(String)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabasePrefix()
+   * @model unique="false"
+   * @generated
+   */
 	String getDatabasePrefix();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabasePrefix <em>Database Prefix</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabasePrefix <em>Database Prefix</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Database Prefix</em>' attribute.
-	 * @see #getDatabasePrefix()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Database Prefix</em>' attribute.
+   * @see #getDatabasePrefix()
+   * @generated
+   */
 	void setDatabasePrefix(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Database Host</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Database Host</b></em>' attribute.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Database Host</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Database Host</em>' attribute.
-	 * @see #setDatabaseHost(String)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseHost()
-	 * @model
-	 * @generated
-	 */
+   * @return the value of the '<em>Database Host</em>' attribute.
+   * @see #setDatabaseHost(String)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseHost()
+   * @model unique="false"
+   * @generated
+   */
 	String getDatabaseHost();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseHost <em>Database Host</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseHost <em>Database Host</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Database Host</em>' attribute.
-	 * @see #getDatabaseHost()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Database Host</em>' attribute.
+   * @see #getDatabaseHost()
+   * @generated
+   */
 	void setDatabaseHost(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Database Name</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Database Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Database Name</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Database Name</em>' attribute.
-	 * @see #setDatabaseName(String)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseName()
-	 * @model
-	 * @generated
-	 */
+   * @return the value of the '<em>Database Name</em>' attribute.
+   * @see #setDatabaseName(String)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseName()
+   * @model unique="false"
+   * @generated
+   */
 	String getDatabaseName();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseName <em>Database Name</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseName <em>Database Name</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Database Name</em>' attribute.
-	 * @see #getDatabaseName()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Database Name</em>' attribute.
+   * @see #getDatabaseName()
+   * @generated
+   */
 	void setDatabaseName(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Database Username</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Database Username</b></em>' attribute.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Database Username</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Database Username</em>' attribute.
-	 * @see #setDatabaseUsername(String)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseUsername()
-	 * @model
-	 * @generated
-	 */
+   * @return the value of the '<em>Database Username</em>' attribute.
+   * @see #setDatabaseUsername(String)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabaseUsername()
+   * @model unique="false"
+   * @generated
+   */
 	String getDatabaseUsername();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseUsername <em>Database Username</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabaseUsername <em>Database Username</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Database Username</em>' attribute.
-	 * @see #getDatabaseUsername()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Database Username</em>' attribute.
+   * @see #getDatabaseUsername()
+   * @generated
+   */
 	void setDatabaseUsername(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Database Password</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Database Password</b></em>' attribute.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Database Password</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Database Password</em>' attribute.
-	 * @see #setDatabasePassword(String)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabasePassword()
-	 * @model
-	 * @generated
-	 */
+   * @return the value of the '<em>Database Password</em>' attribute.
+   * @see #setDatabasePassword(String)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DatabasePassword()
+   * @model unique="false"
+   * @generated
+   */
 	String getDatabasePassword();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabasePassword <em>Database Password</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getDatabasePassword <em>Database Password</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Database Password</em>' attribute.
-	 * @see #getDatabasePassword()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Database Password</em>' attribute.
+   * @see #getDatabasePassword()
+   * @generated
+   */
 	void setDatabasePassword(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Orm Technology</b></em>' attribute.
-	 * The literals are from the enumeration {@link uk.ac.man.cs.mdsd.orm.OrmTechnologies}.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Orm Technology</b></em>' attribute.
+   * The literals are from the enumeration {@link uk.ac.man.cs.mdsd.orm.OrmTechnologies}.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Orm Technology</em>' attribute isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Orm Technology</em>' attribute.
-	 * @see uk.ac.man.cs.mdsd.orm.OrmTechnologies
-	 * @see #setOrmTechnology(OrmTechnologies)
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_OrmTechnology()
-	 * @model
-	 * @generated
-	 */
+   * @return the value of the '<em>Orm Technology</em>' attribute.
+   * @see uk.ac.man.cs.mdsd.orm.OrmTechnologies
+   * @see #setOrmTechnology(OrmTechnologies)
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_OrmTechnology()
+   * @model unique="false"
+   * @generated
+   */
 	OrmTechnologies getOrmTechnology();
 
 	/**
-	 * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getOrmTechnology <em>Orm Technology</em>}' attribute.
-	 * <!-- begin-user-doc -->
+   * Sets the value of the '{@link uk.ac.man.cs.mdsd.orm.OrmModel#getOrmTechnology <em>Orm Technology</em>}' attribute.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Orm Technology</em>' attribute.
-	 * @see uk.ac.man.cs.mdsd.orm.OrmTechnologies
-	 * @see #getOrmTechnology()
-	 * @generated
-	 */
+   * @param value the new value of the '<em>Orm Technology</em>' attribute.
+   * @see uk.ac.man.cs.mdsd.orm.OrmTechnologies
+   * @see #getOrmTechnology()
+   * @generated
+   */
 	void setOrmTechnology(OrmTechnologies value);
 
 	/**
-	 * Returns the value of the '<em><b>Data Types</b></em>' containment reference list.
-	 * The list contents are of type {@link uk.ac.man.cs.mdsd.orm.DataType}.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Data Types</b></em>' containment reference list.
+   * The list contents are of type {@link uk.ac.man.cs.mdsd.orm.DataType}.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Data Types</em>' containment reference list isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Data Types</em>' containment reference list.
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DataTypes()
-	 * @model containment="true"
-	 * @generated
-	 */
+   * @return the value of the '<em>Data Types</em>' containment reference list.
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_DataTypes()
+   * @model containment="true"
+   * @generated
+   */
 	EList<DataType> getDataTypes();
 
 	/**
-	 * Returns the value of the '<em><b>Entities</b></em>' containment reference list.
-	 * The list contents are of type {@link uk.ac.man.cs.mdsd.orm.Entity}.
-	 * It is bidirectional and its opposite is '{@link uk.ac.man.cs.mdsd.orm.Entity#getPartOf <em>Part Of</em>}'.
-	 * <!-- begin-user-doc -->
+   * Returns the value of the '<em><b>Entities</b></em>' containment reference list.
+   * The list contents are of type {@link uk.ac.man.cs.mdsd.orm.Entity}.
+   * It is bidirectional and its opposite is '{@link uk.ac.man.cs.mdsd.orm.Entity#getPartOf <em>Part Of</em>}'.
+   * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Entities</em>' containment reference list isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Entities</em>' containment reference list.
-	 * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_Entities()
-	 * @see uk.ac.man.cs.mdsd.orm.Entity#getPartOf
-	 * @model opposite="partOf" containment="true"
-	 * @generated
-	 */
+   * @return the value of the '<em>Entities</em>' containment reference list.
+   * @see uk.ac.man.cs.mdsd.orm.OrmPackage#getOrmModel_Entities()
+   * @see uk.ac.man.cs.mdsd.orm.Entity#getPartOf
+   * @model opposite="partOf" containment="true"
+   * @generated
+   */
 	EList<Entity> getEntities();
 
 } // OrmModel
