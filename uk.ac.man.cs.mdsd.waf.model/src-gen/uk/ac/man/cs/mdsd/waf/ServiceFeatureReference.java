1236f4eaee1bc9790eccba41dab0c225cb8b53fa
Andy Carpenter <Andy.Carpenter@manchester.ac.uk>
2015-12-09 22:55:12 +0000

    Allow control of page/unit urls

---
+++ uk.ac.man.cs.mdsd.waf.model/src-gen/uk/ac/man/cs/mdsd/waf/ServiceFeatureReference.java
@@ -0,0 +1,18 @@
+/**
+ */
+package uk.ac.man.cs.mdsd.waf;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Service Feature Reference</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see uk.ac.man.cs.mdsd.waf.WafPackage#getServiceFeatureReference()
+ * @model abstract="true"
+ * @generated
+ */
+public interface ServiceFeatureReference extends EObject {
+} // ServiceFeatureReference
