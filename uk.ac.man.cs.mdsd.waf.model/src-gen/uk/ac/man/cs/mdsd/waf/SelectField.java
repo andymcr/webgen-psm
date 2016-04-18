e39f71a57882367642261db8ed00ffa202d117e8
Andy Carpenter <Andy.Carpenter@manchester.ac.uk>
2016-04-18 14:59:06 +0100

    Allow control of select fields on selection queries

---
+++ uk.ac.man.cs.mdsd.waf.model/src-gen/uk/ac/man/cs/mdsd/waf/SelectField.java
@@ -0,0 +1,18 @@
+/**
+ */
+package uk.ac.man.cs.mdsd.waf;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Select Field</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see uk.ac.man.cs.mdsd.waf.WafPackage#getSelectField()
+ * @model abstract="true"
+ * @generated
+ */
+public interface SelectField extends EObject {
+} // SelectField
