466551dabe53d6fe65fc2da24a1f2e7165cd7eab
Andy Carpenter <Andy.Carpenter@manchester.ac.uk>
2017-05-16 15:56:53 +0100

    Remove redundant criteria project

---
+++ uk.ac.man.cs.mdsd.waf.model/src-gen/uk/ac/man/cs/mdsd/waf/ContextMenu.java
@@ -0,0 +1,17 @@
+/**
+ */
+package uk.ac.man.cs.mdsd.waf;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Context Menu</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see uk.ac.man.cs.mdsd.waf.WafPackage#getContextMenu()
+ * @model
+ * @generated
+ */
+public interface ContextMenu extends Menu {
+} // ContextMenu
---
+++ uk.ac.man.cs.mdsd.waf.model/src-gen/uk/ac/man/cs/mdsd/waf/GlobalMenu.java
@@ -0,0 +1,17 @@
+/**
+ */
+package uk.ac.man.cs.mdsd.waf;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Global Menu</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see uk.ac.man.cs.mdsd.waf.WafPackage#getGlobalMenu()
+ * @model abstract="true"
+ * @generated
+ */
+public interface GlobalMenu extends Menu {
+} // GlobalMenu
