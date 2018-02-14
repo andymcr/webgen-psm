f59b61b30dbda348b0c53806e867142560d77e80
andymcr <Andy Carpenter>
2018-02-14 17:08:01 +0000

    Allow control of pagination page size

---
+++ uk.ac.man.cs.mdsd.waf.model/src-gen/uk/ac/man/cs/mdsd/waf/IndexCardsUnit.java
@@ -0,0 +1,17 @@
+/**
+ */
+package uk.ac.man.cs.mdsd.waf;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Index Cards Unit</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see uk.ac.man.cs.mdsd.waf.WafPackage#getIndexCardsUnit()
+ * @model
+ * @generated
+ */
+public interface IndexCardsUnit extends IndexUnit {
+} // IndexCardsUnit
---
+++ uk.ac.man.cs.mdsd.waf.model/src-gen/uk/ac/man/cs/mdsd/waf/impl/IndexCardsUnitImpl.java
@@ -0,0 +1,37 @@
+/**
+ */
+package uk.ac.man.cs.mdsd.waf.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import uk.ac.man.cs.mdsd.waf.IndexCardsUnit;
+import uk.ac.man.cs.mdsd.waf.WafPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Index Cards Unit</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class IndexCardsUnitImpl extends IndexUnitImpl implements IndexCardsUnit {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected IndexCardsUnitImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return WafPackage.Literals.INDEX_CARDS_UNIT;
+	}
+
+} //IndexCardsUnitImpl
