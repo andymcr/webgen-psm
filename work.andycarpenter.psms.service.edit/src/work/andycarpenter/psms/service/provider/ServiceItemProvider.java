b2f32aa91b66a73c01907ae72f4b4f48bfea0191
Andy Carpenter <6350464+andymcr@users.noreply.github.com>
2021-01-30 23:21:32 +0000

    Allow services to references other services and repositories

--- work.andycarpenter.psms.service.edit/src/work/andycarpenter/psms/service/provider/ServiceItemProvider.java
+++ work.andycarpenter.psms.service.edit/src/work/andycarpenter/psms/service/provider/ServiceItemProvider.java
@@ -52,6 +52,7 @@ public class ServiceItemProvider extends NamedElementItemProvider {
 			super.getPropertyDescriptors(object);
 
 			addPartOfPropertyDescriptor(object);
+			addRequiresPropertyDescriptor(object);
 			addUsesPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
@@ -71,6 +72,28 @@ public class ServiceItemProvider extends NamedElementItemProvider {
 				 getString("_UI_Service_partOf_feature"),
 				 getString("_UI_PropertyDescriptor_description", "_UI_Service_partOf_feature", "_UI_Service_type"),
 				 ServicePackage.Literals.SERVICE__PART_OF,
+				 true,
+				 false,
+				 true,
+				 null,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Requires feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addRequiresPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Service_requires_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Service_requires_feature", "_UI_Service_type"),
+				 ServicePackage.Literals.SERVICE__REQUIRES,
 				 true,
 				 false,
 				 true,
